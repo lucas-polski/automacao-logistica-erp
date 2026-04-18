43161a8fd32676f1397e821da6a19b71c3814e95
Lucas Polski <lucaspolski2011@hotmail.com>
2026-04-18 09:26:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -159,6 +159,15 @@ def verificar_e_excluir_itens_outro_estoque(navegador, wait):
     navegador.switch_to.default_content()
     wait.until(EC.frame_to_be_available_and_switch_to_it("cadastro"))
 
+    """"
+
+    Adicionar Clear() após realizar a exclusão 
+    #####################
+    #####################
+    #####################
+
+    """
+
     return produtos_excluidos
 
 
